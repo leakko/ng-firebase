83eb5a291b0ae2f67d78907588b0d48d73fb5003
Marcos Rodriguez <Marcos.Rodriguez@geograma.com>
2022-06-01 20:36:09 +0200

    Styling links

--- src/app/modules/skills/skills.module.ts
+++ src/app/modules/skills/skills.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SkillsComponent } from './skills/skills.component';
+import { SkillsRoutingModule } from './skills-routing-module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SkillsComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    SkillsRoutingModule
   ]
 })
 export class SkillsModule { }
